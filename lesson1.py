7ad724eb5e936037a2ab431286a532a285baa4e2
ksh <hyun031916@gmail.com>
2020-09-08 00:15:46 +0900

    내장 함수를 사용하여 함수를 구현하는 프로그램

--- lesson1.py
+++ lesson1.py
@@ -66,7 +66,7 @@
 # print(movie_rank)
 
 # #10. movie_rank 리스트에서 '럭키'를 삭제하라.
-# movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+# movie_rank = ['닥터 스트레인지', '럭키', '슈퍼맨', '스플릿', '럭키', '배트맨']
 # movie_rank.remove('럭키')
 # print(movie_rank)
 #
