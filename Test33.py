99d867c420b80b47e5338a45cbe2cd916f6747f2
ksh <hyun031916@gmail.com>
2020-11-02 14:55:39 +0900

    Cos-pro 대비 문제 풀이

--- Test33.py
+++ Test33.py
@@ -26,8 +26,8 @@ def func_c(bundle):
 
 
 def solution(n, bundle):
-    a_cards = func_a(bundle, n)[: n]
-    b_cards = func_a(bundle, n)[: n]
+    a_cards = func_a(bundle, 0)[: n]
+    b_cards = func_a(bundle, 1)[: n]
     a_score = func_c(a_cards)
     b_score = func_c(b_cards)
     return func_b(a_score, b_score)
