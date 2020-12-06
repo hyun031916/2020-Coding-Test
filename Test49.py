5ee557010601653842fff1502fe0918b59470bd7
ksh <hyun031916@gmail.com>
2020-12-07 00:52:44 +0900

    Cos-pro 대비 문제 풀이(49~50)

--- Test49.py
+++ Test49.py
@@ -2,13 +2,20 @@
 #import math
 
 def solution(score):
-  # 여기에 코드를 작성해주세요.
-   answer = []
-   return answer
+    # 여기에 코드를 작성해주세요.
+    answer = []
+    answer = [0] * len(score)
+    for i in range(len(score)):
+        answer[i] = sum(map(lambda s: s > score[i], score))+1
+    return answer
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
 score1 = [90, 87, 87, 23, 35, 28, 12, 46]
 ret1 = solution(score1)
+
+#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+score3 = [1, 2, 3, 4, 7, 5, 6, 8, 9]
+ret3 = solution(score3)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은 ", ret1, " 입니다.")
@@ -18,3 +25,7 @@ ret2 = solution(score2)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은 ", ret2, " 입니다.")
+
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은 ", ret3, " 입니다.")
--- Test50.py
+++ Test50.py
@@ -2,14 +2,13 @@
 #import math
 
 def solution(time_table, n):
-   #여기에 코드를 작성해주세요.
-   answer = 0
-   arr = list()
-   for i in range(0, len(time_table)):
-       for j in range(0, n):
-           if(i%n == j):
-               arr[j] += time_table[i]
-   return answer
+    #여기에 코드를 작성해주세요.
+    answer = 0
+    arr = [0 for _ in range(n)]
+    for i, j in enumerate(time_table):
+        arr[i % n] += j
+    answer = max(arr)
+    return answer
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
 time_table1 = [1, 5, 1, 9]
@@ -25,3 +24,11 @@ ret2 = solution(time_table2, n2)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret2, "입니다.")
+
+#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+time_table3 = [3, 7, 4, 10, 8]
+n3 = 5
+ret3 = solution(time_table3, n3)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret3, "입니다.")
